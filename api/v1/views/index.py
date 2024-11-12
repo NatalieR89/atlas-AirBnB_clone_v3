127e80e42d716555ce4c4baf7ce7806ca0814fc6
Cole Kelsey <Cole.Kelsey@atlasschool.com>
2024-11-12 14:14:03 -0600

    redid stat getter

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from models import storage
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def get_status():
     """ Returns JSON """
-    return jsonify({"status": "OK"})
+    return jsonify({'status': 'OK'})
 
 
 @app_views.route('/api/v1/stats', methods=['GET'])
@@ -19,12 +19,14 @@ def get_stats():
     """
     Retrieves the number of each object type by calling storage.count()
     """
-    stats = {
-        "User": storage.count("User"),
-        "Place": storage.count("Place"),
-        "City": storage.count("City"),
-        "Amenity": storage.count("Amenity"),
-        "Review": storage.count("Review"),
-        "State": storage.count("State")
-    }
-    return jsonify(stats)
+    objects = {"amenities": 'Amenity', "cities": 'City',
+               "places": 'Place', "reviews": 'Review',
+               "states": 'State', "users": 'User'}
+    stats = {}
+    try:
+        for key, value in objects.items():
+            stats[key] = storage.count(value)
+            print(f"Count of {value}: {stats[key]}")
+            return jsonify(stats)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500 
