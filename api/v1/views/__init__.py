572ce6df3684951d03c08d61720621a2c366d963
NatalieR89 <natalieritch89@gmail.com>
2024-11-11 18:00:10 -0600

    getting closer

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,8 @@ from flask import Blueprint
 # Create an instance of Blueprint with the URL prefix /api/v1
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import everything from `api.v1.views.index` to avoid circular imports later
-# PEP8 may flag this, but it's necessary to avoid circular import issues
 from api.v1.views.index import *
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.states import *
